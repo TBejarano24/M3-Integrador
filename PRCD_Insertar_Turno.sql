8d7d9ca7abe9cf7313a477411e1d4be03869c903
TBejarano24 <bejaranotomas04@gmail.com>
2025-01-24 00:27:17 -0300

    Create PRCD_Insertar_Turno.sql

---
+++ PRCD_Insertar_Turno.sql
@@ -0,0 +1,49 @@
+--PROCEDIMIENTOS: Insertar Turno
+
+USE ExamenIntegrador
+
+GO
+
+CREATE PROCEDURE InsertarTurno
+	@fechaTurno date,
+	@idEstado smallint,
+	@DNI_Paciente char(10),
+	@DNI_Medico char(10),
+	@observacion varchar(1000) = ''
+
+AS
+BEGIN
+	
+	--Se verifica si el médico introducido existe
+	IF EXISTS (SELECT 1 FROM Medico WHERE DNI = @DNI_Medico)
+	BEGIN
+		
+		--De ser así, se verifica si el paciente introducido existe
+		IF EXISTS (SELECT 1 FROM Paciente WHERE DNI = @DNI_Paciente)
+		BEGIN
+
+			--Si se cumplen ambas condiciones, se insertan los datos
+			--en las tablas pertinentes
+			INSERT INTO Turno (fechaTurno, idEstado, observacion)
+			VALUES (@fechaTurno, @idEstado, @observacion)
+
+			INSERT INTO TurnoPaciente (idTurno, idPaciente, idMedico)
+			VALUES (@@IDENTITY, (SELECT idPaciente FROM Paciente WHERE @DNI_Paciente = Paciente.DNI), (SELECT idMedico FROM Medico WHERE @DNI_Medico = Medico.DNI))
+
+			PRINT 'TURNO AÑADIDO EXITOSAMENTE'
+
+		END
+
+		ELSE
+			--Si el paciente no existe, se muestra una advertencia
+			PRINT 'NO SE ENCONTRÓ AL PACIENTE'
+
+	END
+
+	ELSE
+		--Si el médico no existe, se muestra una advertencia
+		PRINT 'NO SE ENCONTRÓ AL MÉDICO'
+
+END
+
+--EXEC InsertarTurno '2025-02-06', 0, '50200139', '39312094', 'Sin Observaciones'
